8c8b9079b4200a6d9a4446d9ff5c7de77369927e
DenysHnatiuk <denys.hnatiuk@gmail.com>
2018-06-21 17:58:18 +0300

    edit

---
+++ view/serviceworker.js
@@ -0,0 +1,39 @@
+"use strict";
+var urlsToCache = [];
+
+var CACHE_NAME = 'fitness-cache-v1';
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+// Cache name: adjust version number to invalidate service worker cachce.
+var CACHE_NAME = 'fitness-cache-v2';
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    cache.match(event.request).then(function(response) {
+      return response || fetch(event.request).then(function(response) {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    });
+  );
+});
